af79778ea1692db1aea7b128dab637c902a0356e
Nathen Smith <nathencsmith@gmail.com>
2021-08-31 00:29:10 -0500

    adding typescript

---
+++ web/src/Routes.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+export const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/users">
+        <Users />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  );
+};
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
