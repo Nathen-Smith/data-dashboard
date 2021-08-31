fc00cd0abe7479b74a1fddc37918228ff2905932
Nathen Smith <nathencsmith@gmail.com>
2021-08-31 02:49:46 -0500

    input for countries search done, needs dataviz then importing data to firebase

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { db } from "./firebase";
 import { useCollectionData } from "react-firebase-hooks/firestore"; // this dependency does not support latest version of firebase
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import TextField from "@material-ui/core/TextField";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 export default function App() {
-  // const moviesRef = ;
-  // const q = moviesRef.orderBy;
+  const [formInput, setFormInput] = useState("");
+  const [queryText, setQueryText] = useState("");
   const [movies, loading, error] = useCollectionData(
-    db
-      .collection("movies")
-      .where("production_countries.name", "==", "United States of America")
+    db.collection("movies").where("production_countries.name", "==", queryText)
   );
+
   return (
     <div>
-      {movies &&
-        movies.map((movie) => (
-          <React.Fragment key={movie.id}>
-            {JSON.stringify(movie)},{" "}
-          </React.Fragment>
-        ))}
+      <TextField
+        id="standard-basic"
+        onChange={(e) => setFormInput(e.target.value)}
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            setQueryText(formInput);
+          }
+        }}
+        error={!movies && !loading}
+        label="Country"
+      />
+      <br />
+
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <span>
+          {movies &&
+            movies.map((movie) => (
+              <React.Fragment key={movie.id}>
+                {JSON.stringify(movie)},{" "}
+              </React.Fragment>
+            ))}
+        </span>
+      )}
+      {error}
     </div>
   );
 }
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
