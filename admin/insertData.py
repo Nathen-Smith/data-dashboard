123ed052b2044d10fa1b867c027d9284da24ba32
Nathen Smith <nathencsmith@gmail.com>
2021-09-10 04:01:40 -0500

    completed data insert script

--- admin/insertData.py
+++ admin/insertData.py
@@ -1,48 +1,36 @@
 import firebase_admin, asyncio, os, datetime, csv
 from firebase_admin import credentials, firestore
 
+DATE_IDX = 14
+
 if not firebase_admin._apps:
     credential_path = './serviceAccountKey.json'
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
     cred = credentials.Certificate(credential_path) 
     default_app = firebase_admin.initialize_app(cred)
 
-async def add_data_types(test):
-    db = firestore.AsyncClient()
-    # [START firestore_data_set_from_map_nested_async]
-    data = {
-        "stringExample": "Hello, World!",
-        "booleanExample": True,
-        "numberExample": 3.14159265,
-        "dateExample": datetime.datetime(1995, 10, 30), # need to convert this one.
-        "arrayExample": [5, True, "hello"],
-        "nullExample": None,
-        # "objectExample": {"a": 5, "b": True},
-        "production_countries":{"name":"India"},
-        "belongs_to_collection": test
-    }
+def process_data(movie_data):
+    processed_data = []
+    for idx, element in enumerate(movie_data):
+        if idx == DATE_IDX and element:
+            processed_data.append(datetime.datetime(int(element[0:4]), int(element[5:7]), int(element[8:10])))
+        else:
+            try:
+                processed_data.append(eval(element))
+            except:
+                processed_data.append(element)
+    return processed_data
 
-    await db.collection('movies').add(data)
-    # [END firestore_data_set_from_map_nested_async]
+async def add_data_types(data_types, movie_data):
+    db = firestore.AsyncClient()
+    processed_data = dict(zip(data_types, process_data(movie_data)))
+    await db.collection('movies').add(processed_data)
 
 async def main():
-    # await add_data_types()
-    # db = firestore.AsyncClient()
-    # users_ref = db.collection('movies')
-    # docs = users_ref.stream()
-
-    # async for doc in docs:
-    #     print(f'{doc.id} => {doc.to_dict()}')
     with open('./assets/movies_metadata.csv', newline='') as csvfile:
         reader = csv.reader(csvfile)
-        # row1 = next(reader) # data names
-        row2 = next(reader)
-        # spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        # for row in spamreader:
-        #     print(', '.join(row))
-        await add_data_types(eval(row2[1])) # need eval or else it will all be strings
-        # may need asyncio.gather
-
-    # datatypes = ['adult', 'belongs_to_collection', 'budget', 'genres', 'homepage', 'id', 'imdb_id', 'original_language', 'original_title', 'overview', 'popularity', 'poster_path', 'production_companies', 'production_countries', 'release_date', 'revenue', 'runtime', 'spoken_languages', 'status', 'tagline', 'title', 'video', 'vote_average', 'vote_count']
+        data_types = next(reader)
+        for row in reader:
+            await add_data_types(data_types, row)
 
 asyncio.run(main())
