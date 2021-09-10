74efe49ac3c4a7a45fa2613ab793aa08f7b9f4f0
Nathen Smith <nathencsmith@gmail.com>
2021-09-10 13:27:04 -0500

    autopep8 formatting

--- admin/insert_data.py
+++ admin/insert_data.py
@@ -1,4 +1,8 @@
-import firebase_admin, asyncio, os, datetime, csv
+import firebase_admin
+import asyncio
+import os
+import datetime
+import csv
 from firebase_admin import credentials, firestore
 
 PROD_COUNTRIES_IDX = 13
@@ -7,8 +11,9 @@ DATE_IDX = 14
 if not firebase_admin._apps:
     credential_path = './serviceAccountKey.json'
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
-    cred = credentials.Certificate(credential_path) 
+    cred = credentials.Certificate(credential_path)
     default_app = firebase_admin.initialize_app(cred)
+
 
 def process_data(movie_data):
     processed_data = []
@@ -16,9 +21,11 @@ def process_data(movie_data):
         if idx == PROD_COUNTRIES_IDX and element:
             # for ease of querying production countries, just get array of country names
             prod_countries = eval(element)
-            processed_data.append([prod_country['name'] for prod_country in prod_countries])
+            processed_data.append([prod_country['name']
+                                  for prod_country in prod_countries])
         elif idx == DATE_IDX and element:
-            processed_data.append(datetime.datetime(int(element[0:4]), int(element[5:7]), int(element[8:10])))
+            processed_data.append(datetime.datetime(
+                int(element[0:4]), int(element[5:7]), int(element[8:10])))
         else:
             try:
                 processed_data.append(eval(element))
@@ -26,10 +33,12 @@ def process_data(movie_data):
                 processed_data.append(element)
     return processed_data
 
+
 async def add_data_types(data_types, movie_data):
     processed_data = dict(zip(data_types, process_data(movie_data)))
     db = firestore.AsyncClient()
     await db.collection('movies').add(processed_data)
+
 
 async def main():
     with open('./assets/movies_metadata.csv', newline='') as csvfile:
@@ -39,7 +48,7 @@ async def main():
         for row in reader:
             await add_data_types(data_types, row)
             limit = limit + 1
-            if limit == 30000:
+            if limit == 15000:
                 break
 
 asyncio.run(main())
